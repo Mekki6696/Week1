c2fd55b99c0d3e8719539bf93b4106daecb0d99c
Mekki6696 <mekki6696@outlook.com>
2023-04-24 10:42:00 +0100

    script code

---
+++ script.js
@@ -0,0 +1,19 @@
+var consolelog = ("hello there")
+// console.log(consolelog.toUpperCase())
+// console.log(consolelog.toLocaleLowerCase())
+// console.log(consolelog.trim())
+console.log(Math.random())
+//genarates random number between 0 and 1  
+// console.log(Math.random()* 10)
+//genarates a random number between 1 and 10 
+// console.log(Math.random()* 10)
+console.log(Math.round(Math.random()* 10))
+//Math.round rounds the intreger into a whole number
+// console.log("   |   |   |")
+// console.log("   |   |   |")
+// console.log("   ---------")
+// console.log("   |   |   |")
+// console.log("   |   |   |")
+// console.log("   ---------")
+// console.log("   |   |   |")
+// console.log("   |   |   |")
