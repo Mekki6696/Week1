d3039e618a099ee3fb5d2326edfc4c5cae64f566
Mekki6696 <mekki6696@outlook.com>
2023-04-24 10:35:02 +0100

    challenges

---
+++ challenges.js
@@ -0,0 +1,57 @@
+// cha 1
+// const favDrinks = [
+//     "tea",
+//     "coffee",
+//     "latte",
+//     "coke",
+//     "hot choclate"
+// ]
+
+// favDrinks.splice(0,2)
+// console.log(favDrinks)
+
+
+// cha 2
+// let ranMath = Math.ceil(Math.random()*80)+1
+// console.log(ranMath)
+
+
+// cha3
+// let roll = (Math.floor(Math.random()*6)+ 1)
+// console.log (`Rolled: ${roll}`)
+
+
+// cha4
+// code keeps outputting both if staments can not see why?
+// const password = ["jayden"]
+
+// const i = [password.length]
+
+// if (i < 6) {
+//     console.log("password to short")
+// } 
+// else if (i => 6);{
+//         console.log (`${i.length} thank you`)
+// }
+
+
+// cha 5 
+// let bool1 = true
+// let bool2 = false
+// if (bool1 || bool2 === true){
+//     console.log("value is true")
+// }
+
+
+// cha 6 
+let num = 10 
+
+if (num % 3){
+    console.log("fizz")
+}
+else if (num % 5){
+    console.log("buzz")
+}
+else if ( num % 3 && 5 ){
+    console.log("fizz buzz")
+}
