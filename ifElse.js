cc53728716753c59b5f010c712f3c601da9873e7
Mekki6696 <mekki6696@outlook.com>
2023-04-24 10:37:17 +0100

    ifElse

---
+++ ifElse.js
@@ -0,0 +1,114 @@
+// let weather = "rain"
+// //condition is false
+
+// if(weather == "sunny"){
+//     console.log("it's sunny outside")
+// } else if
+//     (weather == "rain"){
+//     console.log("i hate this rain")
+// } else{
+//     console.log("hate this weather")
+// }
+
+
+
+
+// let age = 12
+
+// if(age < 12){console.log("can see PG movie")
+// } 
+//     else if 
+//     (age < 15){
+//         console.log("can see PG & 12")
+//     }
+//         else if( age < 18){ console.log("can see PG, 12 & 15")
+//         }
+//             else{console.log("can view all movies")
+//             }
+
+
+
+
+// let num = "1"
+// if(num=== "1"){
+//     console.log("same value, same data type")
+// }else {
+//     console.log("diffrent for both")
+// }
+
+
+
+// let place ="Liverpool"
+// let weather = "rain"
+// if (place=="Liverpool"&& weather=="sunny"){
+//     console.log(" Are you sure??")
+// } else if (place=="Liverpool" && weather=="snow"){
+//     console.log("Thats more like it!")
+// }else if(place=="Liverpool"&&weather=="rain"){
+//     console.log("Typical")
+// } else{
+//     console.log("What is happening!?")
+// }
+
+
+
+
+//activity 1
+let age = 21
+let country = "United Kingdom"
+if (age < 18 && country == "United Kingdom"){
+    console.log("You are not old enough")
+} else if( age >= 18 && country == "United Kingdom"){
+    console.log("Yes i can serve you")
+} else {
+    console.log("Please show me your id")
+}
+
+
+
+
+// //activity 2 
+// //switch
+// const pizzaTopping1 = "spicey" 
+// const pizzaTopping2 = "pinapple"
+// const pizzaTopping3 = "chicken"
+
+// switch (pizzaTopping1,pizzaTopping2,pizzaTopping3){
+//     case "spicey":
+//         case "chicken":
+//             console.log("yes please!")
+//             break;
+//             case "veg":
+//                 case "tomato":
+//                     console.log("no thank you")
+//                     break;
+//                     case "beef":
+//                         case "fish":
+//                             console.log("Thats discusting")
+//                             break;
+//                             default:
+//                                 console.log("can i just have chese");
+                
+// }
+
+
+
+//activity 3 
+
+
+
+
+
+
+//activity 4
+// let num = 3
+
+// if ( num % 5===0 && num % 3===0){
+//     console.log("divisible by 5 and 3 ")
+// } else if ( num % 3 === 0){
+//     console.log("divisible by 3 ")
+// } else if (num % 5 === 0){
+//     console.log("divisible by 5")
+// }else {
+//     console.log("not disvisible by 5 or 3")
+// }
