fc65104528d98d7de8af9973305684d9da1fcf94
Mekki6696 <mekki6696@outlook.com>
2023-04-24 10:43:51 +0100

    variables code

---
+++ variables.js
@@ -0,0 +1,43 @@
+// console.log ("all around the world".charAt(7).toUpperCase())
+//   let firstName = "khalif"
+//   //let is a veriable and it can be changed 
+//   const surname = "mekki"
+//   // const is a variable and it cannot be changed
+//   firstName = "batman"
+
+
+
+
+
+
+// let num1 = 2
+// let num2 = 3
+// num1 = num1 * 3
+// //let result = num1 * num2
+// num2++
+// //++ adds 1 
+// //-- takes away one 
+// console.log (num1)
+// console.log(num2)
+// num1+=1
+// console.log (num1)
+
+
+
+
+
+
+let favouriteColour = "Blue"
+let HomeTown = "Liverpool"
+console.log ("my favourite colour is " + favouriteColour, "I live in " + HomeTown)
+
+
+
+
+
+// let today = new Date()
+// let birthday = new Date()
+// birthday.setDate(6)
+// birthday.setMonth(6)
+// let time = birthday.getTime() - today.getTime()
+// console.log (time/(1000 * 60 * 60 * 24))
